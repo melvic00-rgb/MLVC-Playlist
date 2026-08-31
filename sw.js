f99b6c09883b8e3218e6614f8f893df3166bb179
melvic00-rgb <melvicsaindal00@gmail.com>
2026-08-31 11:44:24 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,83 +1,43 @@
-const CACHE_NAME = "melvic-music-v1";
+const CACHE_NAME = "mlvc-playlist-v1";
 
 const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./script.js",
-    "./manifest.json"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 
-    event.waitUntil(
-
-        caches.open(CACHE_NAME)
-            .then(cache => {
-
-                return cache.addAll(
-                    FILES_TO_CACHE
-                );
-
-            })
-
-    );
-
-    self.skipWaiting();
-
+  self.skipWaiting();
 });
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
 
-self.addEventListener("activate", event => {
-
-    event.waitUntil(
-
-        caches.keys().then(keys => {
-
-            return Promise.all(
-
-                keys
-                    .filter(
-                        key =>
-                            key !== CACHE_NAME
-                    )
-                    .map(
-                        key =>
-                            caches.delete(key)
-                    )
-
-            );
-
-        })
-
-    );
-
-    self.clients.claim();
-
+  self.clients.claim();
 });
 
-
-self.addEventListener("fetch", event => {
-
-    event.respondWith(
-
-        caches.match(event.request)
-            .then(cached => {
-
-                return (
-                    cached ||
-                    fetch(event.request)
-                );
-
-            })
-            .catch(() => {
-
-                return caches.match(
-                    "./index.html"
-                );
-
-            })
-
-    );
-
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
